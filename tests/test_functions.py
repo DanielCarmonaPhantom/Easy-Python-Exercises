99410e0d06e423de5e9b87ff8d83784c265e4e91
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-13 22:39:57 -0500

    Se termino los ejemplos

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -22,7 +22,64 @@ class Function_testing:
         if self.resultado == self.esperado:
             print(f'¡Superaste la prueba {prueba}!')
         else: 
-            print(f'Test {prueba }no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+    
+    def test_excercise_four(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
+    def test_excercise_five(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
+    def test_excercise_six(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+    
+    def test_excercise_seven(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
+    def test_excercise_eight(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
+    def test_excercise_nine(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
+    def test_excercise_ten(self, resultado, esperado, prueba):
+        self.resultado = resultado
+        self.esperado = esperado
+        if self.resultado == self.esperado:
+            print(f'¡Superaste la prueba {prueba}!')
+        else: 
+            print(f'Test {prueba } no existoso \nSe esperaba {self.esperado} y tu resultado fue {self.resultado}' )
+
 
 
 
